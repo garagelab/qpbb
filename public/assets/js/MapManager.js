681c3b17259c1ed44eb06e6ec8db4b2ca9443559
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2012-12-01 18:30:15 -0300

    Layer de empresas

--- public/assets/js/MapManager.js
+++ public/assets/js/MapManager.js
@@ -2,6 +2,7 @@ var MapManager = function MapManager(options) {
 
     this.options = options;
     this.map = null;
+    this.layers = {};
 
     this.init = function() {
         var self = this;
@@ -13,6 +14,19 @@ var MapManager = function MapManager(options) {
             mapTypeId: self.options.googleMapsOptions.mapTypeId
         };
         this.map = new google.maps.Map(document.getElementById(self.options.mapDivId), googleMapOptions);
+    }
+
+    this.addFTLayer = function(name, ftId, locationColumn) {
+        var self = this;
+
+        var layer = new google.maps.FusionTablesLayer({
+            query: {
+                select : locationColumn,
+                from: ftId
+            },
+            map: self.map
+        });
+        self.layers[name] = layer;
     }
 
     this.init();
