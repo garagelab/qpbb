062732886fd7855cde8bf8d0fac1c8bd1d200414
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2012-12-19 10:09:45 -0300

    Navegacion funcional

--- public/assets/js/qpbb.js
+++ public/assets/js/qpbb.js
@@ -14,10 +14,44 @@ var Qpbb = function Qpbb(mapManager) {
                 $(this).parent().addClass("current");
 
                 var section = $(this).attr("data-section");
+                var currentView = $(".view.current");
+
                 if (section == "salud") {
-                    self.mapManager.setMapType("salud");
+                    if(!$('#map-view').hasClass("current")) {
+                        currentView
+                            .removeClass('current')
+                            .fadeOut('fast', function() {
+                                $('#map-view')
+                                    .fadeIn('fast')
+                                    .addClass('current');
+                                self.mapManager.setMapType("salud");
+                            })
+                    } else {
+                        self.mapManager.setMapType("salud");
+                    }
                 } else if (section == "denuncias") {
-                    self.mapManager.setMapType("denuncias");
+                    if(!$('#map-view').hasClass("current")) {
+                        currentView
+                            .removeClass('current')
+                            .fadeOut('fast', function() {
+                                $('#map-view')
+                                    .fadeIn('fast')
+                                    .addClass('current');
+                                self.mapManager.setMapType("denuncias");
+                            })
+                    } else {
+                        self.mapManager.setMapType("denuncias");
+                    }
+                } else if (section == "aire") {
+                    if(!$('#aqi-view').hasClass("current")) {
+                        currentView
+                            .removeClass('current')
+                            .fadeOut('fast', function() {
+                                $('#aqi-view')
+                                    .fadeIn('fast')
+                                    .addClass('current');
+                            })
+                    }
                 }
             }
 
